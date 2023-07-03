80a36950eaed79c6aed90c0de61c26ae16d0b91b
ManiKandan2129 <137371444+ManiKandan2129@users.noreply.github.com>
2023-07-04 00:19:42 +0530

    day 5 task

---
+++ day-5/task 2.js
@@ -0,0 +1,51 @@
+//create your own resume dat in JSON format:
+
+obj={
+    "basics": {
+      "name": "mani kandan",
+      "label": "Full Stack Devloper",
+      "email": "gokuvirat2129@gmail.com",
+      "phone": "8668067425",
+      "summary": "A summary of manikandanr",
+      "location": {
+        "address": "nagercoil",
+        "postalCode": "629002",
+        "city": "tamilnadu",
+        "countryCode": "India",
+        "region": "Union Teritory"
+      },
+     
+    },
+    
+    "education": [{
+      "institution": "scott college",
+      "area": "mca",
+      "studyType": "masters",
+      "startDate": "2020-6-22",
+      "endDate": "2022-05-12",
+      "score": "7.9",
+    }],
+    
+    "skills": [{
+      "name": "Web Development",
+      "level": "Begineer",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript"
+      ]
+    }],
+    "languages": [{
+      "language": "English,Tamil",
+     
+    }],
+    "interests": [{
+      "name": "cricket"
+    }],
+    "projects": [{
+      "name": "Project1",
+      "description": "Smart Agriculture System Using Iot and Machine Learning"},{"name1":"Project2","description2":"Event Management Application" } ],
+      "declaration":{"descp":"I here by declare that the above stated information is true to the best of my knowledge and belief"}
+  };
+  console.log(obj);
+  
